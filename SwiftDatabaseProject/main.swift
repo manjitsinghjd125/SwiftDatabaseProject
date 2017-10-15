9471d188b7a498c124eb4a6e1e5cffc054cc3a86
Randeep Singh <Randeep@Randeeps-MacBook-Pro.local>
2017-10-15 15:30:43 -0400

    Initial Commit

---
+++ SwiftDatabaseProject/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SwiftDatabaseProject
+//
+//  Created by Randeep Singh on 15/10/17.
+//  Copyright © 2017 Randeep Singh. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
